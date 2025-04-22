99be00c04dbcf0749340b1a41d1ab596bda3e04d
linzhe141 <1572213544@qq.com>
2025-04-22 10:14:56 +0800

    build: to monorepo

--- playground/mcpup.config.ts
+++ playground/mcpup.config.ts
@@ -6,9 +6,6 @@ const __dirname = path.dirname(__filename)
 export default [
   {
     name: "hackernews",
-    serverFilePath: path.resolve(
-      __dirname,
-      "./playground/server/hackernews-server.ts"
-    ),
+    serverFilePath: path.resolve(__dirname, "./server/hackernews-server.ts"),
   },
 ]
